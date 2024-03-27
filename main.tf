36701fec99b88ea06f5b4d38b41d578e04cba626
Clodagh Walsh <clodaghwalsh17@gmail.com>
2024-03-27 12:11:00 +0000

    Add main.tf
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.53.0"
+    }
+  }
+}
+
+provider "openstack" {
+  cloud = "openstack"
+}
+
+resource "openstack_compute_instance_v2" "terraform-test" {
+  name            = "terraform-test"
+  image_name      = "cirros-0.6.2-x86_64-disk"
+  flavor_id       = "2"
+  security_groups = ["default"]
+
+  network {
+    name = "public"    
+  }
+}
