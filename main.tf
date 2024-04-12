9e56fbc6c631817f6dd6aea3ee79ad7d7382b500
Clodagh Walsh <clodaghwalsh17@gmail.com>
2024-04-12 14:50:22 +0100

    Add network and router
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- main.tf
+++ main.tf
@@ -29,8 +29,36 @@ resource "openstack_compute_instance_v2" "flux-terraform-test" {
   security_groups = ["default"]
 
   network {
-    name = "ext_net"
+    uuid = openstack_networking_network_v2.openshift-private.id
   }
+}
+
+resource "openstack_networking_network_v2" "openshift-private" {
+  name           = "flux-deployment-openshift"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "nodes" {
+  name            = "flux-deployment-openshift-nodes"
+  cidr            = "10.0.0.0/16"
+  ip_version      = 4
+  network_id      = openstack_networking_network_v2.openshift-private.id
+  dns_nameservers = "10.1.6.1"
+}
+
+resource "openstack_networking_router_v2" "openshift-external-router" {
+  name                = "flux-deployment-external-router"
+  admin_state_up      = true
+  external_network_id = "7b490a0c-0f5c-4475-a436-e4bcbecc7f5e"
+  external_fixed_ip {
+    subnet_id = "16bd151c-10e9-4aa0-87b4-9625e203a942"
+    ip_address = "10.1.6.63"
+  }
+}
+
+resource "openstack_networking_router_interface_v2" "nodes_router_interface" {
+  router_id = openstack_networking_router_v2.openshift-external-router.id
+  subnet_id = openstack_networking_subnet_v2.nodes.id
 }
 
 output "hello_world" {
